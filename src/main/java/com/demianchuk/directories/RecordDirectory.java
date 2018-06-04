6acb195b01b089eb2890f917f9bff7f8b448e67c
Nick <nicksetone@gmail.com>
2018-06-04 13:32:40 -0500

    Add removeAllRecords and getAllRecords

--- src/main/java/com/demianchuk/directories/RecordDirectory.java
+++ src/main/java/com/demianchuk/directories/RecordDirectory.java
@@ -48,12 +48,12 @@ public class RecordDirectory {
                 .collect(Collectors.toList());
     }
 
+    public List<Record> getAllRecords() {
+        return records;
+    }
 
-
-
-
-
-
-
+    public void removeAllRecords() {
+        records.clear();
+    }
 
 }
